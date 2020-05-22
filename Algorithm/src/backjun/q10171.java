a214ae95c594a2ad318c147823fac98a4dbbe6ba
jaeryo2357 <jaeryo2357@naver.com>
2020-05-22 11:50:59 +0900

    고양이/곱셈

---
+++ Algorithm/src/backjun/q10171.java
@@ -0,0 +1,11 @@
+package backjun;
+
+public class q10171 {
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
