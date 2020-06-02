9f90ef8fe771ca15bf6caf8242a2ebda063d4fe4
jaeryo2357 <jaeryo2357@naver.com>
2020-06-02 12:52:03 +0900

    제곱근과 에라토스테네스의 체

--- Algorithm/src/backjun/q1929.java
+++ Algorithm/src/backjun/q1929.java
@@ -9,13 +9,41 @@ public class q1929 {
         int m = scanner.nextInt();
         int n = scanner.nextInt();
 
-        for (int t = m; t <= n; t++) {
-            if (isPrime(t)) {
-                System.out.println(t);
+        eratos(m, n);
+
+//        for (int t = m; t <= n; t++) {
+//            if (isPrime(t)) {
+//                System.out.println(t);
+//            }
+//        }
+    }
+
+    public static void eratos(int start, int end) {
+        boolean[] ranges = new boolean[end]; //기본값 false
+
+        for (int t = 1; t <= end; t++) {
+            if (ranges[t - 1]) {
+                continue;
+            }
+            if (t == 1) {
+                ranges[t - 1] = true;
+                continue;
+            }
+            for (int i = t + t; i <= end; i += t) {
+                if (i % t == 0) {
+                    ranges[i - 1] = true;
+                }
+            }
+        }
+
+        for (int t = start - 1; t < ranges.length; t++) {
+            if (!ranges[t]) {
+                System.out.println(t + 1);
             }
         }
     }
 
+    //제곱근으로 소수를 구하는 방법
     public static boolean isPrime(int number) {
         if (number == 1) {
             return false;
