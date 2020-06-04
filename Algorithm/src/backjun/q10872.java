d5704c449c646649f6ba60cb18760ce9a7599eb6
jaeryo2357 <jaeryo2357@naver.com>
2020-06-04 14:01:33 +0900

    팩토리얼

---
+++ Algorithm/src/backjun/q10872.java
@@ -0,0 +1,20 @@
+package backjun;
+
+import java.util.Scanner;
+
+public class q10872 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        System.out.println(factorial(n));
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+}
