2bdb937e5092fa43cc044c396b92257dce9f8169
jaeryo2357 <jaeryo2357@naver.com>
2020-06-08 11:57:44 +0900

    수 정렬하기

---
+++ Algorithm/src/backjun/q2750.java
@@ -0,0 +1,30 @@
+package backjun;
+
+import java.util.Scanner;
+
+public class q2750 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}
