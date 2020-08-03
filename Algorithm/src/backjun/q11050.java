76be901f037bac3021fb6372f70cebd8833b7af5
jaeryo2357 <jaeryo2357@naver.com>
2020-08-03 13:31:04 +0900

    이항계수 1

---
+++ Algorithm/src/backjun/q11050.java
@@ -0,0 +1,19 @@
+package backjun;
+
+import java.util.Scanner;
+
+public class q11050 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        System.out.println(factorial(n) / (factorial(k) * factorial(n - k)));
+    }
+
+    public static int factorial(int n) {
+        if (n == 1) return 1;
+
+        return n * factorial(n - 1);
+    }
+}
